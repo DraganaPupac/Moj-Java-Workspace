dc7dcd822b6429339a2be8f8b578470d442a4e65
DraganaPupac <dragana.pupac@gmail.com>
2016-12-13 12:12:41 +0100

    test

---
+++ Dragana02-TipoviPodataka/src/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+	public static void main(String[] args) {
+int myNumber = 1;
+short myShort = 2;
+long myLong = 9898;
+double myDouble = 23;
+boolean myBoolean = true;
+
+System.out.println(myNumber);
+System.out.println(myShort);
+System.out.println(myLong);
+System.out.println(myDouble);
+System.out.println(myNumber);
+System.out.println(myBoolean);
+
+	// TODO Auto-generated method stub
+
+}
+
+}
